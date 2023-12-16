a36d8d33519ec090875f2525690ccb3a10a51b46
Dylan <dylanosbornebuisness@gmail.com>
2023-12-16 16:24:45 +0200

    Changes made to the rules script

--- src/Components/JSX/Rules.jsx
+++ src/Components/JSX/Rules.jsx
@@ -6,8 +6,39 @@ const Rules = () => {
     return (
         <div className="background">
             <div className="container d-flex align-items-center justify-content-center vh-100">
-                <div className="backgroundReverse border border-5 border-dark p-5 bg bg-primary">
+                <div className="backgroundReverse border border-5 border-dark p-4 bg bg-primary">
+
+                    <h2 className="text-center text-light border border-5 border-light p-3 ps-4 pe-4 bg-black">Selected Mode</h2>
+
+                    <div className="text-light border border-5 border-light p-3 ps-4 pe-4 bg-black mb-2">
+                        <h4 className="text-center">Rules</h4>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    </div>
+
+                    <div className="text-light border border-5 border-light p-3 ps-4 pe-4 bg-black mb-2">
+                        <h4 className="text-center">Please Select</h4>
+
+                        <div className='container d-flex justify-content-center'>
+                            <input className='col-2' type='radio' name='cardType'/>Numbers
+                            <input className='col-2' type='radio' name='cardType'/>Emoji's
+                            <input className='col-2' type='radio' name='cardType'/>Colors
+                        </div>
+                    </div>
+
+                    <div>
+                        <h4 className="text-center text-light border border-5 border-light p-2 ps-4 pe-4 bg-black mb-2">Board Sizes</h4>
+
+                        <div className='container d-flex justify-content-center'>
+                            <button className='col-3 btn btn-dark border-3 border-light p-2 bg-black me-2 me-sm-1'>Numbers</button>
+                            <button className='col-3 btn btn-dark border-3 border-light p-2 bg-black me-2 me-sm-1 ms-2 ms-sm-1'>Emoji's</button>
+                            <button className='col-3 btn btn-dark border-3 border-light p-2 bg-black ms-2 ms-sm-1'>Colors</button>
+                        </div>
+                    </div>
                 
+                    <Link to="/SelectGameMode" className="mt-4 btn btn-dark bg-black btn-lg border-5 border-light d-flex align-items-center justify-content-center" role="button">Back</Link>
                 </div>
             </div>
         </div>
--- src/Components/JSX/SelectGameMode.jsx
+++ src/Components/JSX/SelectGameMode.jsx
@@ -20,17 +20,17 @@ const SelectGameMode = () => {
                     <div className="container p-2">
                         <div className='row justify-content-center'>
                             <div className='col-4'>
-                                <Link to="/" onClick={() => handleModeSelection('timed')} 
+                                <Link to="/Rules" onClick={() => handleModeSelection('timed')} 
                                 className="mt-4 btn btn-dark border-2 border-light bg-black border-light d-flex align-items-center justify-content-center p-4" role="button">Timed</Link>
                             </div>
 
                             <div className='col-4'>
-                                <Link to="/" onClick={() => handleModeSelection('memory')} 
+                                <Link to="/Rules" onClick={() => handleModeSelection('memory')} 
                                 className="mt-4 btn btn-dark border-2 border-light bg-black border-light d-flex align-items-center justify-content-center p-4" role="button">Memory</Link>
                             </div>
 
                             <div className='col-4'>
-                                <Link to="/" onClick={() => handleModeSelection('standard')} 
+                                <Link to="/Rules" onClick={() => handleModeSelection('standard')} 
                                 className="mt-4 btn btn-dark border-2 border-light bg-black border-light d-flex align-items-center justify-content-center p-4" role="button">Standard</Link>
                             </div>
                         </div>
