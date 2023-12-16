439f9c0236ae1be05cc24f808ac7aa8b5d52773c
Dylan <dylanosbornebuisness@gmail.com>
2023-12-16 11:10:48 +0200

    Added basic start screen

---
+++ src/Components/StartScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const StartScreen = () => {
+    return (
+        <>
+            <title>Card Memory Game</title>
+        
+            <div>
+                <div>
+                    <div>
+                        <h1>Card Memory Game</h1>
+                        <h1>Welcome</h1>
+                        <h1>Simple game for all ages</h1>
+                    </div>
+        
+                    <div>
+                        <a href='#'>Continue</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+  };
+  
+  export default StartScreen;
