8d83d4bb0585a3101ce5ed736818c2940fe7ae89
Dylan <dylanosbornebuisness@gmail.com>
2023-12-16 11:54:56 +0200

    Added bootstrap CSS to the StartScreen

--- src/Components/StartScreen.js
+++ src/Components/StartScreen.js
@@ -2,23 +2,19 @@ import React from 'react';
 
 const StartScreen = () => {
     return (
-        <>
-            <title>Card Memory Game</title>
-        
-            <div>
-                <div>
-                    <div>
-                        <h1>Card Memory Game</h1>
-                        <h1>Welcome</h1>
-                        <h1>Simple game for all ages</h1>
+        <div className="bg-warning">
+            <div className="container d-flex align-items-center justify-content-center vh-100">
+                <div className="border border-5 border-dark p-5 bg bg-primary">
+                    <div className="text-center text-light border border-5 border-light p-4 bg bg-dark">
+                        <h1 className="mb-4">Card Memory Game</h1>
+                        <h1 className="mb-4">Welcome</h1>
+                        <h1 className="mb-4">Simple game for all ages</h1>
                     </div>
-        
-                    <div>
-                        <a href='#'>Continue</a>
-                    </div>
+
+                    <a href="#" className="mt-4 btn btn-dark btn-lg border border-5 border-light d-flex align-items-center justify-content-center" role="button">Continue</a>
                 </div>
             </div>
-        </>
+        </div>
     );
   };
   
